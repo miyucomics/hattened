d16bbd0a314378e215f9ad78a23550926c393c2a
miyucomics <nbriannyc@gmail.com>
2025-08-01 23:04:03 -0400

    Initial commit

---
+++ src/client/java/miyucomics/hattened/HattenedClient.kt
@@ -0,0 +1,9 @@
+package miyucomics.hattened
+
+import net.fabricmc.api.ClientModInitializer
+
+object HattenedClient : ClientModInitializer {
+	override fun onInitializeClient() {
+
+	}
+}
---
+++ src/main/java/miyucomics/hattened/HattenedMain.kt
@@ -0,0 +1,14 @@
+package miyucomics.hattened
+
+import net.fabricmc.api.ModInitializer
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
+
+object HattenedMain : ModInitializer {
+    @JvmField
+	val LOGGER: Logger = LoggerFactory.getLogger("hattened")
+
+	override fun onInitialize() {
+
+	}
+}
